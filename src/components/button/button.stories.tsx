7c6306052f83a6d2390c6d20802e9a8a0c0e4d96
Jisang Hong <jisang1024@gmail.com>
2024-08-22 22:55:02 +0900

    버튼 컴포넌트의 기본적인 스토리북 구성

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./button";
+
+const meta = {
+  title: "Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    children: {
+      control: "text",
+      name: "레이블",
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: "레이블",
+  },
+};
