812e45396e5d01ba2583617248e6ce2d5fdfcc31
Jisang Hong <jisang1024@gmail.com>
2024-09-03 23:45:00 +0900

    Basic setup

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,7 +1,27 @@
+import styled from "styled-components";
+
 interface ButtonProps {
   children: string;
+  size?: "md" | "lg";
+  loading?: boolean;
+  disabled?: boolean;
+  icon?: boolean;
+  danger?: boolean;
 }
 
+const StyledButton = styled.button`
+  border: 1px;
+  border-radius: 8px;
+  border-color: #28a5ff;
+  color: white;
+  background-color: #58b5fe;
+  font-family: "Pretendard";
+
+  height: 38px;
+  padding: 8px 12px;
+  font-size: 16px;
+`;
+
 export default function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+  return <StyledButton>{children}</StyledButton>;
 }
