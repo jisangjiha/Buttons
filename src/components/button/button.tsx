1319aff2f2dd36d4d7f240c9c6612c3c71f4f914
jisangjiha <97737608+jisangjiha@users.noreply.github.com>
2024-09-06 21:24:03 +0900

    Styled component (#3)
    * install styled-components
    
    * Basic setup
    
    * ThemeProvider 사용
    
    * 스타일 수정
    
    border, line-height

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,7 +1,34 @@
+import styled, { ThemeProvider } from "styled-components";
+import theme from "../Theme";
+
 interface ButtonProps {
   children: string;
+  size?: "md" | "lg";
+  loading?: boolean;
+  disabled?: boolean;
+  icon?: boolean;
+  danger?: boolean;
 }
 
+const StyledButton = styled.button`
+  border: 1px solid ${(props) => props.theme.primary400};
+  border-radius: 8px;
+  color: white;
+  background-color: ${(props) => props.theme.primary300};
+  font-family: "Pretendard";
+
+  line-height: 20px;
+
+  padding: 8px 12px;
+  font-size: 16px;
+`;
+
 export default function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <StyledButton>{children}</StyledButton>
+      </ThemeProvider>
+    </>
+  );
 }
