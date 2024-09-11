e20ba13db906fb27af36a0d0e08c4e137a1fe2cb
jisangjiha <97737608+jisangjiha@users.noreply.github.com>
2024-09-11 21:01:58 +0900

    button-pseudo 및 box-shadow 추가 (#6)
    * button-pseudo 및 box-shadow 추가
    
    * 스타일 수정

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -11,16 +11,45 @@ interface ButtonProps {
 }
 
 const StyledButton = styled.button`
-  border: 1px solid ${(props) => props.theme.primary400};
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${(props) => props.theme.primary400};
   border-radius: 8px;
+  padding: 8px 12px;
+
   color: white;
   background-color: ${(props) => props.theme.primary300};
-  font-family: "Pretendard";
+
+  box-shadow: 0 1px 4px rgba(17, 21, 24, 0.2);
 
   line-height: 20px;
 
-  padding: 8px 12px;
+  font-family: "Pretendard";
   font-size: 16px;
+
+  &:hover {
+    border-color: ${(props) => props.theme.primary600};
+    background-color: ${(props) => props.theme.primary500};
+    box-shadow: 0 2px 5px rgba(17, 21, 24, 0.2);
+    cursor: pointer;
+  }
+
+  &:focus-visible {
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+    outline: ${(props) => props.theme.primaryFocus} solid 2px;
+  }
+
+  &:active {
+    border-color: ${(props) => props.theme.primary700};
+    background-color: ${(props) => props.theme.primary600};
+    box-shadow: 0 1px 2px rgba(17, 21, 24, 0.2);
+  }
+
+  &:disabled {
+    border-color: ${(props) => props.theme.gray500};
+    background-color: ${(props) => props.theme.gray400};
+    cursor: not-allowed;
+  }
 `;
 
 export default function Button({ children }: ButtonProps) {
