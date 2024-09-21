c2201370efda26a49f6db1a1806e6f18dfc86be6
Jisang Hong <jisang1024@gmail.com>
2024-09-21 15:36:23 +0900

    danger 스타일 관리를 위한 getColor 함수 생성

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import styled, { ThemeProvider } from "styled-components";
+import styled, { DefaultTheme, ThemeProvider } from "styled-components";
 import theme from "../Theme";
 
 interface ButtonProps {
@@ -10,15 +10,25 @@ interface ButtonProps {
   danger?: boolean;
 }
 
+function getColor(
+  props: Required<ButtonProps> & { theme: DefaultTheme },
+  number: number
+) {
+  if (props.danger) {
+    return props.theme[`danger${number}`];
+  }
+  return props.theme[`primary${number}`];
+}
+
 const StyledButton = styled.button<Required<ButtonProps>>`
   border-width: 1px;
   border-style: solid;
-  border-color: ${(props) => props.theme.primary400};
+  border-color: ${(props) => getColor(props, 400)};
   border-radius: 8px;
   padding: ${(props) => (props.size === "md" ? "8px 12px" : "12px 16px")};
 
   color: white;
-  background-color: ${(props) => props.theme.primary300};
+  background-color: ${(props) => getColor(props, 300)};
 
   box-shadow: 0 1px 4px rgba(17, 21, 24, 0.2);
 
@@ -29,8 +39,8 @@ const StyledButton = styled.button<Required<ButtonProps>>`
   font-size: ${(props) => (props.size === "md" ? "16px" : "24px")};
 
   &:hover {
-    border-color: ${(props) => props.theme.primary600};
-    background-color: ${(props) => props.theme.primary500};
+    border-color: ${(props) => getColor(props, 600)};
+    background-color: ${(props) => getColor(props, 500)};
     box-shadow: 0 2px 5px rgba(17, 21, 24, 0.2);
     cursor: pointer;
   }
@@ -41,8 +51,8 @@ const StyledButton = styled.button<Required<ButtonProps>>`
   }
 
   &:active {
-    border-color: ${(props) => props.theme.primary700};
-    background-color: ${(props) => props.theme.primary600};
+    border-color: ${(props) => getColor(props, 700)};
+    background-color: ${(props) => getColor(props, 600)};
     box-shadow: 0 1px 2px rgba(17, 21, 24, 0.2);
   }
 
@@ -54,24 +64,12 @@ const StyledButton = styled.button<Required<ButtonProps>>`
   }
 
   &.danger {
-    border-color: ${(props) => props.theme.danger400};
-    background-color: ${(props) => props.theme.danger300};
-    &:hover {
-      border-color: ${(props) => props.theme.danger600};
-      background-color: ${(props) => props.theme.danger500};
-    }
     &:focus-visible {
       outline: ${(props) => props.theme.dangerFocus} solid 2px;
-    }
-    &:active {
-      border-color: ${(props) => props.theme.danger700};
-      background-color: ${(props) => props.theme.danger600};
     }
     &:disabled {
       border-color: ${(props) => props.theme.danger300};
       background-color: ${(props) => props.theme.danger200};
-      cursor: not-allowed;
-      box-shadow: none;
     }
   }
 `;
