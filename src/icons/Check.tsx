b330fdde4d27498b7ddb74a490f98611dbb629db
Jisang Hong <jisang1024@gmail.com>
2024-09-27 17:38:56 +0900

    install svgr

---
+++ src/icons/Check.tsx
@@ -0,0 +1,19 @@
+import type { SVGProps } from "react";
+const SvgCheck = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4.167 10.833 7.5 14.167l8.333-8.334"
+    />
+  </svg>
+);
+export default SvgCheck;
